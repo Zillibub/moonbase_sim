8d399e1f4f6516e86c25fe00878fff6dfb8f4820
Serj <serj2work@gmail.com>
2023-08-28 14:49:08 +0100

    Added case # 3

--- evaluation_cases/case_1/expected_code.py
+++ evaluation_cases/case_1/expected_code.py
@@ -2,30 +2,36 @@ from moonbase_sim.sensor import Sensor, SensorMetadata
 from moonbase_sim.moonbase import MoonBase
 from moonbase_sim.message import Message
 
-moonbase = MoonBase("BASE-001")
 
-sensor1 = Sensor(
-    sensor_id="TEMP-001",
-    sensor_metadata=SensorMetadata(
-        measured_value="Temperature",
-        units="Celsius",
-        type="Thermometer",
-        calibration_date="2021-01-01",
-        description="Measures the temperature"
+def main():
+    moonbase = MoonBase("BASE-001")
+
+    sensor1 = Sensor(
+        sensor_id="TEMP-001",
+        sensor_metadata=SensorMetadata(
+            measured_value="Temperature",
+            units="Celsius",
+            type="Thermometer",
+            calibration_date="2021-01-01",
+            description="Measures the temperature"
+        )
     )
-)
-sensor2 = Sensor(
-    sensor_id="MAG-001",
-    sensor_metadata=SensorMetadata(
-        measured_value="Magnetic Field",
-        units="Tesla",
-        type="Magnetometer",
-        calibration_date="2021-01-01",
-        description="Measures the magnetic field")
-)
+    sensor2 = Sensor(
+        sensor_id="MAG-001",
+        sensor_metadata=SensorMetadata(
+            measured_value="Magnetic Field",
+            units="Tesla",
+            type="Magnetometer",
+            calibration_date="2021-01-01",
+            description="Measures the magnetic field")
+    )
 
-moonbase.add_sensor(sensor1)
-moonbase.add_sensor(sensor2)
+    moonbase.add_sensor(sensor1)
+    moonbase.add_sensor(sensor2)
 
-message = Message(content="Sensors added: Temperature Sensor, Magnetometer")
-moonbase.send_message(message)
+    message = Message(content="Sensors added: Temperature Sensor, Magnetometer")
+    moonbase.send_message(message)
+
+
+if __name__ == "__main__":
+    main()
--- evaluation_cases/case_3/preparation.py
+++ evaluation_cases/case_3/preparation.py
@@ -1,20 +1,17 @@
 from moonbase_sim.sensor import Sensor, SensorMetadata
 from moonbase_sim.moonbase import MoonBase
-import random
-from datetime import datetime, timedelta
-
-moonbase = MoonBase("BASE-001")
-
-start_date = datetime(2021, 1, 1)
-end_date = datetime(2021, 12, 31)
-
-
-def random_date(start_date, end_date):
-    return start_date + timedelta(
-        seconds=random.randint(0, int((end_date - start_date).total_seconds())))
 
 
 def main():
+    dates = [
+        "2021-02-17", "2021-07-15", "2021-09-11", "2021-05-02", "2021-03-29",
+        "2021-04-07", "2021-01-23", "2021-11-01", "2021-08-16", "2021-11-09",
+        "2021-07-22", "2021-04-08", "2021-02-03", "2021-03-12", "2021-08-24",
+        "2021-11-26", "2021-09-13", "2021-05-19", "2021-10-06", "2021-12-21"
+    ]
+
+    moonbase = MoonBase("BASE-001")
+
     # Create 10 temperature sensors
     for i in range(1, 11):
         sensor = Sensor(
@@ -23,7 +20,7 @@ def main():
                 measured_value="Temperature",
                 units="Celsius",
                 type="Thermometer",
-                calibration_date=random_date(start_date, end_date).strftime("%Y-%m-%d"),
+                calibration_date=dates[i - 1],
                 description=f"Temperature Sensor {i}"
             )
         )
@@ -37,11 +34,13 @@ def main():
                 measured_value="Magnetic Field",
                 units="Tesla",
                 type="Magnetometer",
-                calibration_date=random_date(start_date, end_date).strftime("%Y-%m-%d"),
+                calibration_date=dates[i + 9],
                 description=f"Magnetometer Sensor {i}"
             )
         )
         moonbase.add_sensor(sensor)
+
+    moonbase.save_state("inital_state.json")
 
 
 if __name__ == "__main__":
