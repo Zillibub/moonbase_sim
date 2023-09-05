d30b2c290c3266027ec01f890e7e10b1c52bff9e
Serj <serj2work@gmail.com>
2023-09-05 21:27:05 +0100

    Added init

--- evaluation_cases/__init__.py
+++ evaluation_cases/__init__.py
@@ -1,9 +1,11 @@
 from evaluation_cases.case_1 import case as case_1
 from evaluation_cases.case_2 import case as case_2
 from evaluation_cases.case_3 import case as case_3
+from evaluation_cases.case_4 import case as case_4
 
 cases = [
     case_1,
     case_2,
     case_3,
+    case_4
 ]
