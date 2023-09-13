61697d8415c2120937ec7b7ed26c9bd2e1b24b84
Serj <serj2work@gmail.com>
2023-09-13 21:03:48 +0100

    Fixed tests

--- evaluation_cases/__init__.py
+++ evaluation_cases/__init__.py
@@ -1,13 +1,13 @@
-from evaluation_cases.case_1 import case as case_1
-from evaluation_cases.case_2 import case as case_2
-from evaluation_cases.case_3 import case as case_3
-from evaluation_cases.case_4 import case as case_4
-from evaluation_cases.case_5 import case as case_5
+from evaluation_cases.case_1 import init_case as case_1
+from evaluation_cases.case_2 import init_case as case_2
+from evaluation_cases.case_3 import init_case as case_3
+from evaluation_cases.case_4 import init_case as case_4
+from evaluation_cases.case_5 import init_case as case_5
 
 cases = [
-    case_1,
-    case_2,
-    case_3,
-    case_4,
-    case_5
+    case_1(),
+    case_2(),
+    case_3(),
+    case_4(),
+    case_5()
 ]
