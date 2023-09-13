9a1abaec03cf5402ac5d21a920f807d592a77e95
Serj <serj2work@gmail.com>
2023-09-13 21:38:16 +0100

    Added method to load codebase

--- evaluator/base_evaluator.py
+++ evaluator/base_evaluator.py
@@ -1,4 +1,5 @@
 import re
+import os
 import subprocess
 import threading
 import queue
@@ -15,9 +16,11 @@ class BaseEvaluator:
             output_path: Path,
             case_id: int,
             evaluation_cases_path: Path,
+            codebase_path: Path,
     ):
         self.output_path = output_path
         self.evaluation_cases_path = evaluation_cases_path
+        self.codebase_path = codebase_path
         self.case_id = case_id
 
     @staticmethod
@@ -50,6 +53,19 @@ class BaseEvaluator:
             output_path=str(self.output_path)
         )
         return prompt
+
+    def load_codebase(self):
+        output = ""
+        for root, dirs, files in os.walk(self.codebase_path):
+            for file in files:
+                if file.endswith('.py'):
+                    file_path = os.path.join(root, file)
+                    with open(file_path, 'r') as f:
+                        file_content = f.read()
+                        output += f"file_path: {file_path.replace(str(self.codebase_path), '')} | file_content: {file_content} ||"
+            with open(self.evaluation_cases_path / f"case_{self.case_id}" / "codebase", 'r') as f:
+                codebase = f.read()
+        return codebase
 
     @abstractmethod
     def generate_code(self):
@@ -86,5 +102,3 @@ class BaseEvaluator:
         validation_module.validate_state(state, expected_state)
 
         return False
-
-
